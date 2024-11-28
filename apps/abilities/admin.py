833716084940e019157168deaf43e424f4ff8a63
Rafael Soriano Ramírez <rafa.soriano.ramirez@gmail.com>
2024-11-28 17:25:06 -0600

    [Add]: abilities

--- apps/abilities/admin.py
+++ apps/abilities/admin.py
@@ -3,6 +3,5 @@ from .models import Ability
 
 @admin.register(Ability)
 class AbilityAdmin(admin.ModelAdmin):
-    # Muestra los atributos actuales del modelo Ability
     list_display = ("id", "name", "description", "short_effect")
     search_fields = ("name", "description", "short_effect")
